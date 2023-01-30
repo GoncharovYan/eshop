6cec786a49b89f8a5edc5d01f93f03f4ec3d0d6a
Носарев Евгений <nightpetrol@yandex.ru>
2023-01-30 17:05:41 +0300

    Initial commit

---
+++ public/index.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once '../src/Kernel.php';
+
+use App\Kernel;
+
+$app = new Kernel();
+$app->run();
---
+++ src/Kernel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+class Kernel
+{
+    public function run()
+    {
+        echo 'Page not found';
+    }
+}
