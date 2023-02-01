fa52897d0b4a3bef8b33a9367cb50a69397cb2da
вашÐAngelinaKhomutova <gelya.khomutova.03@gmail.com>
2023-02-01 14:13:37 +0200

    make main view

---
+++ src/View/Items/layout.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="/public/resources/css/style-main.css">
+	<title>Team&3-Eshop</title>
+</head>
+<body>
+	<header>
+		<div class="container">
+			<a href=""><img src="/../../public/resources/images/logo.png" width="190px" alt=""></a>
+			<div class="search">
+				<label>
+					<input type="search" value="Поиск по сайту" class="input_text">
+				</label>
+				<button><img src="/../../public/resources/images/icon-search.png" height="23px" width="22px" alt=""></button>
+			</div>
+			<a><img src="/../../public/resources/images/icon-user.png" width="35px"></a>
+		</div>
+<!--		подумать над шириной-->
+
+<!--		<nav class="header-links">Главная</nav>-->
+	</header>
+	<hr>
+	<main>
+		<div class="tags">
+			<ul>
+				<a href="#"><li>#Электроника</li></a>
+				<a href="#"><li>#Ноутбуки</li></a>
+			</ul>
+		</div>
+		<div class="catalog">
+			<div class="item">
+				<img src="" height="165px" width="195px">
+				<p class="name">Название</p>
+				<span>Описание</span>
+				<p class="price">Цена</p>
+			</div>
+			<div class="item">
+				<img src="" height="165px" width="195px">
+				<p class="name">Название</p>
+				<span>Описание</span>
+				<p class="price">Цена</p>
+			</div>
+			<div class="item">
+				<img src="" height="165px" width="195px">
+				<p class="name">Название</p>
+				<span>Описание</span>
+				<p class="price">Цена</p>
+			</div>
+			<div class="item">
+				<img src="" height="165px" width="195px">
+				<p class="name">Название</p>
+				<span>Описание</span>
+				<p class="price">Цена</p>
+			</div>
+			<div class="item">
+				<img src="" height="165px" width="195px">
+				<p class="name">Название</p>
+				<span>Описание</span>
+				<p class="price">Цена</p>
+			</div>
+			<div class="item">
+				<img src="" height="165px" width="195px">
+				<p class="name">Название</p>
+				<span>Описание</span>
+				<p class="price">Цена</p>
+			</div>
+		</div>
+	</main>
+<!--	Для погинации-->
+	<div></div>
+	<footer>
+	</footer>
+</body>
+</html>
