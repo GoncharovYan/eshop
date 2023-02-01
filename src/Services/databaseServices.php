9dfe23da8d6ed1e076dd20d3bcb9dc060dc32f0f
Носарев Евгений <nightpetrol@yandex.ru>
2023-02-01 18:25:22 +0300

    Fix Services

--- src/Services/databaseServices.php
+++ src/Services/databaseServices.php
@@ -28,5 +28,3 @@ function getDbConnection()
 	}
 	return $connection;
 }
-
-$connection = getDbConnection();
--- src/Services/index.php
+++ src/Services/index.php
@@ -1,6 +1,4 @@
 <?php
 
-const ROOT = __DIR__;
-
 require_once ROOT . '/src/Services/configurationServices.php';
 require_once ROOT . '/src/Services/databaseServices.php';
