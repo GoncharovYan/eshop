086e2e77cb95ed775e2f9942800b17cf1a62d69e
Goncharov Yan <pigfatski@gmail.com>
2023-02-03 13:20:16 +0200

    Router added Paths added

--- src/App/Kernel.php
+++ src/App/Kernel.php
@@ -4,19 +4,27 @@ namespace App;
 
 use Core\Database\Migration\Migrator;
 use Controller\CatalogController;
+use Core\Routing\Router;
 
 class Kernel
 {
     public function run()
     {
-		$environment = 'nodev';
+        $route = Router::find($_SERVER['REQUEST_METHOD'], $_SERVER['REQUEST_URI']);
+
+		$environment = 'dev';
 		// DROP ALL TABLES BEFORE START MIGRATION!
 		if($environment === 'dev')
 		{
 			Migrator::migrate();
 		}
 
-        $app = new CatalogController();
-        $app->catalogPage();
+        if ($route)
+        {
+            $action = $route->action;
+            $variables = $route->getVariables();
+
+            $action(...$variables);
+        }
     }
 }
