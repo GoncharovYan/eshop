4f7cb70ffa992259600c56f001bd683120835ca2
вашÐAngelinaKhomutova <gelya.khomutova.03@gmail.com>
2023-02-16 22:49:35 +0200

    update detail page

---
+++ src/Controller/public/ErrorController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Controller\public;
+
+use Controller\BaseController;
+
+class ErrorController extends BaseController
+{
+	public function pageNotFound(){
+		echo $this->render('layoutView.php', [
+				'content' => $this->render('public/pageNotFoundView.php', [
+
+				]),
+		]);
+	}
+}
