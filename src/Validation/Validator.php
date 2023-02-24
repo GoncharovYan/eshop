33fdf418278e30e9e581c34e4a2d40166b8b06b6
Ruslan <ruslan040506@gmail.com>
2023-02-24 12:02:25 +0200

    Validator fix

--- src/Validation/Validator.php
+++ src/Validation/Validator.php
@@ -57,10 +57,10 @@ class Validator
 		if (!is_numeric($number) || !is_int($number + 0)) {
 			$this->errors []= "$name must be only numeric";
 		}
-		if (is_null($max) && $number > $max) {
+		if (!is_null($max) && $number > $max) {
 			$this->errors [] = "$name is more than the maximum";
 		}
-		if (is_null($min) && $number < $min) {
+		if (!is_null($min) && $number < $min) {
 			$this->errors [] = "$name is less than the minimum";
 		}
 	}
